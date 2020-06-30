98ce4e2efdb4ace6be8ff725e045e33b73ba7792
Ram Paranjothy <ram.paranjothy@amd.com>
2020-06-30 17:24:51 -0500

    show on g

--- client/test.js
+++ client/test.js
@@ -3,3 +3,35 @@ let ccx = () => {
   w.onopen = (e) => console.log(e);
   w.onmessage = (m) => console.log(JSON.parse(m.data));
 };
+
+let ccx = () => {
+  let w = new WebSocket("ws://raceday-staging.sppo:9001");
+  // w.onopen = (e) => console.log(e);
+  w.onmessage = (m) => {
+    console.log(JSON.parse(m.data));
+    const ll = document.createElement("div");
+
+    ll.id = "guid-list";
+
+    const mv = document.querySelector("#user-content");
+    mv.style.display = "flex";
+
+    mv.style.justifyContent = "center";
+
+    // console.log(mv);
+    mv.innerHTML = "";
+    const d = JSON.parse(m.data);
+    d.forEach((g) => {
+      const aGuid = document.createElement("div");
+      aGuid.style.margin = "5px";
+
+      aGuid.style.padding = "10px";
+      aGuid.style.textAlign = "center";
+
+      aGuid.innerText = g.guid;
+      ll.appendChild(aGuid);
+    });
+    mv.appendChild(ll);
+  };
+};
+ccx();
