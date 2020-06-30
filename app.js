b55b2a4d904d77e4f8932682863578785120eadf
Ram Paranjothy <ram.paranjothy@amd.com>
2020-06-29 22:52:35 -0500

    Dockerized

--- app.js
+++ app.js
@@ -20,11 +20,9 @@ expressServer.get("/", (req, res) =>
 
 socketServer.on("connection", (s) => {
   const msg = [s.id, "joined!"].join(" ");
-  // log(msg);
-  // s.emit("msg", "welcome !! " + s.id);
   s.emit("msg", "welcome !! ");
   socketServer.emit("msg", msg);
   s.on("reply", (reply) => socketServer.emit("chat", reply));
 });
 
-httpServer.listen(5555, () => log("Server Started!!"));
+httpServer.listen(process.env.PORT || 8080, () => log("Server Started!!"));
